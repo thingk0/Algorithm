89c54106102ac016e08e5c122e4d8c0ddae56b72
고명성 <75971566+thingk0@users.noreply.github.com>
2025-01-20 13:02:16 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 564 ms, Memory: 73116 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
@@ -1,43 +1,49 @@
+import java.io.*;
 import java.util.ArrayList;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
     
     static int N;
-    static boolean[] visited;
-    static ArrayList<Integer>[] tree;
     static int[] answer;
+    static ArrayList<Integer>[] tree;
     
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        N = sc.nextInt();
-        visited = new boolean[N + 1];
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        N = Integer.parseInt(br.readLine());
+        
         tree = new ArrayList[N + 1];
         answer = new int[N + 1];
         
-        for (int i = 0; i <= N; i++) { // 인덱스 0부터 N까지 초기화
+        for (int i = 0; i <= N; i++) {
             tree[i] = new ArrayList<>();
         }
         
-        for (int i = 1; i < N; i++) { // N-1개의 간선 입력
-            int n1 = sc.nextInt();
-            int n2 = sc.nextInt();
+        for (int i = 1; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int n1 = Integer.parseInt(st.nextToken());
+            int n2 = Integer.parseInt(st.nextToken());
             tree[n1].add(n2);
             tree[n2].add(n1);
         }
         
-        dfs(1); // 1번 노드를 루트로 DFS 시작
-        for (int i = 2; i <= N; i++) { // 2번 노드부터 부모 노드 출력
-            System.out.println(answer[i]);
+        dfs(1);
+        
+        StringBuilder sb = new StringBuilder();
+        for (int i = 2; i <= N; i++) {
+            sb.append(answer[i]).append('\n');
         }
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
     }
     
     static void dfs(int i) {
-        visited[i] = true;
         for (int j : tree[i]) {
-            if (!visited[j]) {
-                answer[j] = i; // j의 부모 노드는 i
-                dfs(j); // 다음 노드 j로 이동
+            if (answer[j] == 0) {
+                answer[j] = i;
+                dfs(j);
             }
         }
     }
