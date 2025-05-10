3186c133cb5da65d7f6aec1598037cf233715d49
고명성 <75971566+thingk0@users.noreply.github.com>
2025-05-10 23:11:03 +0900

    [level 2] Title: 배달, Time: 6.18 ms, Memory: 83.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/12978. 배달/배달.java
@@ -0,0 +1,51 @@
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.PriorityQueue;
+
+class Solution {
+    
+    private static final int INF = Integer.MAX_VALUE;
+    private static class Node {
+        int dest, cost;
+        Node(int dest, int cost) {
+            this.dest = dest; 
+            this.cost = cost;
+        }
+    }
+    
+    public int solution(int N, int[][] road, int K) {
+        ArrayList<Node>[] adjList = new ArrayList[N + 1];
+        for (int i = 1; i <= N; i++) {
+            adjList[i] = new ArrayList<>();
+        }
+        
+        for (int[] edge : road) {
+            adjList[edge[0]].add(new Node(edge[1], edge[2]));
+            adjList[edge[1]].add(new Node(edge[0], edge[2]));
+        }
+        
+        int[] dist = new int[N + 1];
+        Arrays.fill(dist, INF);
+        
+        PriorityQueue<Node> pq = new PriorityQueue<>((o1, o2) -> Integer.compare(o1.cost, o2.cost));
+        pq.add(new Node(1, 0));
+        dist[1] = 0;
+        
+        while (!pq.isEmpty()) {
+            Node now = pq.poll();
+            
+            if (dist[now.dest] < now.cost) {
+                continue;
+            }
+            
+            for (Node next : adjList[now.dest]) {
+                if (dist[next.dest] > now.cost + next.cost) {
+                    dist[next.dest] = now.cost + next.cost;
+                    pq.add(new Node(next.dest, dist[next.dest]));
+                }
+            }
+        }
+        
+        return (int) Arrays.stream(dist).filter(d -> d <= K).count();
+    }
+}
