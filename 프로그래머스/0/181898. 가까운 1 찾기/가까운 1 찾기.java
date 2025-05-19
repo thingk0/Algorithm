28dd782836f087e167e3466a7df1bab91674b311
고명성 <75971566+thingk0@users.noreply.github.com>
2025-05-20 00:03:54 +0900

    [level 0] Title: 가까운 1 찾기, Time: 5.61 ms, Memory: 73.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181898. 가까운 1 찾기/가까운 1 찾기.java
@@ -0,0 +1,10 @@
+import java.util.stream.*;
+
+class Solution {
+    public int solution(int[] arr, int idx) {
+        return IntStream.range(idx, arr.length)
+                .filter(i -> arr[i] == 1)
+                .findFirst()
+                .orElse(-1);
+    }
+}
